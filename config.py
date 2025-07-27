aed5085b5de7a96d2202bcc17edd4d2c0562340e
Zaplavs <Zaplavs@yandex.ru>
2025-07-27 16:20:56 +0300

    a

--- config.py
+++ config.py
@@ -7,6 +7,12 @@ class Settings(BaseSettings):
     SECRET_KEY: str
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+    # Новые настройки для базы данных
+    DB_HOST: str = "localhost"
+    DB_PORT: str = "5432"
+    DB_NAME: str
+    DB_USER: str
+    DB_PASSWORD: str
 
     class Config:
         env_file = ".env"
